06cd39a7da1fffdbb00113a0bb2eebd3cf79fb5a
ian.louzada <ian.louzada@tindin.com.br>
2023-03-06 13:38:55 -0300

    color picker for different parts

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -3,19 +3,14 @@ import Phaser from '../lib/phaser.js'
 export default class Game extends Phaser.Scene {
   constructor() {
     super('game')
+
+    this.color1 = 0xFFFFFF
+    this.color2 = 0xFFFFFF
+
+    this.colorState = 0
   }
 
   preload() {
-    // this.load.image('clothing_top_0_img', '/assets/clothing_top_0.png')
-    // this.load.image('clothing_top_1_img', '/assets/clothing_top_1.png')
-    // this.load.image('clothing_top_2_img', '/assets/clothing_top_2.png')
-    // this.load.image('body_img', '/assets/body.png')
-    // this.load.image('leg0_img', '/assets/leg0.png')
-    // this.load.image('leg2_img', '/assets/leg2.png')
-    // this.load.image('arm2_img', '/assets/arm2.png')
-    // this.load.image('arm0_img', '/assets/arm0.png')
-
-
     this.load.spritesheet('clothing_top_0', '/assets/clothing_top_0.png', { frameWidth: 150, frameHeight: 220 })
     this.load.spritesheet('clothing_top_1', '/assets/clothing_top_1.png', { frameWidth: 150, frameHeight: 220 })
     this.load.spritesheet('clothing_top_2', '/assets/clothing_top_2.png', { frameWidth: 150, frameHeight: 220 })
@@ -52,13 +47,14 @@ export default class Game extends Phaser.Scene {
     this.createHUD()
 
     this.input.on('pointerup', () => {
-      if (this.previourColor !== this.color) this.createVisualization()
+      if (this.prevcolor2 !== this.color2 ||
+        this.prevColor1 !== this.color1) this.createVisualization()
     })
   }
 
   createColorPicker() {
-    this.rexUI.add.colorPicker({
-      x: 750, y: 300,
+    this.colorPicker = this.rexUI.add.colorPicker({
+      x: 750, y: 320,
       background: this.rexUI.add.roundRectangle(0, 0, 0, 0, 10, 0x424242),
       svPalette: {
         width: 400,
@@ -74,8 +70,17 @@ export default class Game extends Phaser.Scene {
       valuechangeCallback: (value) => {
         const pickerColor = new Phaser.Display.Color.IntegerToColor(value)
         const tintColor = new Phaser.Display.Color.RGBStringToColor(`rgb(${pickerColor.b},${pickerColor.g},${pickerColor.r})`)
-        this.color = tintColor.color
-        if (!this.previourColor) this.createVisualization()
+
+        if (this.colorState === 0) {
+          this.color2 = tintColor.color
+          this.color1 = tintColor.color
+        } else if (this.colorState === 1) {
+          this.color1 = tintColor.color
+        } else if (this.colorState === 2) {
+          this.color2 = tintColor.color
+        }
+
+        if (!this.prevColor1) this.createVisualization()
       },
       value: 0xFFFFFF
     }).layout()
@@ -136,12 +141,71 @@ export default class Game extends Phaser.Scene {
       .on('pointerout', () => {
         prevAnimText.setBackgroundColor('#333333')
       })
+
+    const allText = this.add.text(600, 70, 'TUDO', {
+      backgroundColor: (this.colorState !== 0) ? '#333333' : '#555555',
+      color: '#FFFFFF',
+      padding: 20
+    })
+      .setOrigin(0.5)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.colorState = 0
+        bodyText.setBackgroundColor('#333333')
+        sleevesText.setBackgroundColor('#333333')
+      })
+      .on('pointerover', () => {
+        allText.setBackgroundColor('#555555')
+      })
+      .on('pointerout', () => {
+        if (this.colorState !== 0) allText.setBackgroundColor('#333333')
+      })
+
+    const bodyText = this.add.text(689, 70, 'TRONCO', {
+      backgroundColor: (this.colorState !== 1) ? '#333333' : '#555555',
+      color: '#FFFFFF',
+      padding: 20
+    })
+      .setOrigin(0.5)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.colorState = 1
+        allText.setBackgroundColor('#333333')
+        sleevesText.setBackgroundColor('#333333')
+      })
+      .on('pointerover', () => {
+        bodyText.setBackgroundColor('#555555')
+      })
+      .on('pointerout', () => {
+        if (this.colorState !== 1) bodyText.setBackgroundColor('#333333')
+      })
+
+    const sleevesText = this.add.text(788, 70, 'MANGAS', {
+      backgroundColor: (this.colorState !== 2) ? '#333333' : '#555555',
+      color: '#FFFFFF',
+      padding: 20
+    })
+      .setOrigin(0.5)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.colorState = 2
+        bodyText.setBackgroundColor('#333333')
+        allText.setBackgroundColor('#333333')
+      })
+      .on('pointerover', () => {
+        sleevesText.setBackgroundColor('#555555')
+      })
+      .on('pointerout', () => {
+        if (this.colorState !== 2) sleevesText.setBackgroundColor('#333333')
+      })
   }
 
   createVisualization() {
     this.visualization?.destroy()
 
-    this.previourColor = this.color
+    this.prevcolor2 = this.color2
+    this.prevColor1 = this.color1
+    this.prev2Color = this.color2
     const animName = this.animNames[this.currAnimIndex]
 
     const frames0 = this.anims.generateFrameNames('clothing_top_0')
@@ -154,18 +218,18 @@ export default class Game extends Phaser.Scene {
         renderTexture.drawFrame('body', i)
         renderTexture.drawFrame('leg2', i)
         renderTexture.drawFrame('arm2', i)
-        renderTexture.drawFrame('clothing_top_0', i, 0, 0, 1, this.color)
-        renderTexture.drawFrame('clothing_top_1', i, 0, 0, 1, this.color)
-        renderTexture.drawFrame('clothing_top_2', i, 0, 0, 1, this.color)
+        renderTexture.drawFrame('clothing_top_1', i, 0, 0, 1, this.color1)
+        renderTexture.drawFrame('clothing_top_0', i, 0, 0, 1, this.color2)
+        renderTexture.drawFrame('clothing_top_2', i, 0, 0, 1, this.color2)
       } else {
         renderTexture.drawFrame('arm0', i)
-        renderTexture.drawFrame('clothing_top_0', i, 0, 0, 1, this.color)
+        renderTexture.drawFrame('clothing_top_0', i, 0, 0, 1, this.color2)
         renderTexture.drawFrame('leg0', i)
         renderTexture.drawFrame('body', i)
-        renderTexture.drawFrame('clothing_top_1', i, 0, 0, 1, this.color)
+        renderTexture.drawFrame('clothing_top_1', i, 0, 0, 1, this.color1)
         renderTexture.drawFrame('leg2', i)
         renderTexture.drawFrame('arm2', i)
-        renderTexture.drawFrame('clothing_top_2', i, 0, 0, 1, this.color)
+        renderTexture.drawFrame('clothing_top_2', i, 0, 0, 1, this.color2)
       }
 
       if (this.textures.exists(`frame${i}`)) this.textures.remove(`frame${i}`)
@@ -190,18 +254,18 @@ export default class Game extends Phaser.Scene {
     const img = this.make.image({ key: 'clothing_top_1', frame: '__BASE' }, false).setOrigin(0)
     const sprite = this.make.sprite({ key: 'frame0', frame: '__BASE' }, false).setOrigin(0).setScale(4)
 
-    const poseTexture = this.add.renderTexture(0, 0, sprite.width, sprite.height).setVisible(false).setScale(4) 
+    const poseTexture = this.add.renderTexture(0, 0, sprite.width, sprite.height).setVisible(false).setScale(4)
     poseTexture.drawFrame('arm0', 64)
-    poseTexture.drawFrame('clothing_top_0', 64, 0, 0, 1, this.color)
+    poseTexture.drawFrame('clothing_top_0', 64, 0, 0, 1, this.color2)
     poseTexture.drawFrame('leg0', 64)
     poseTexture.drawFrame('body', 64)
-    poseTexture.drawFrame('clothing_top_1', 64, 0, 0, 1, this.color)
+    poseTexture.drawFrame('clothing_top_1', 64, 0, 0, 1, this.color1)
     poseTexture.drawFrame('leg2', 64)
     poseTexture.drawFrame('arm2', 64)
-    poseTexture.drawFrame('clothing_top_2', 64, 0, 0, 1, this.color)
+    poseTexture.drawFrame('clothing_top_2', 64, 0, 0, 1, this.color2)
     poseTexture.saveTexture('poseTexture')
-    
-    const poseSprite = this.make.sprite({key: 'poseTexture'}, false).setScale(4).setOrigin(0)
+
+    const poseSprite = this.make.sprite({ key: 'poseTexture' }, false).setScale(4).setOrigin(0)
 
     const exportTexture0 = this.add.renderTexture(0, 0, img.width, img.height).setVisible(false)
     const exportTexture1 = this.add.renderTexture(0, 0, img.width, img.height).setVisible(false)
@@ -209,18 +273,18 @@ export default class Game extends Phaser.Scene {
     const exportTexture3 = this.add.renderTexture(0, 0, img.width, img.height).setVisible(false)
     const exportTexture4 = this.add.renderTexture(0, 0, sprite.displayWidth, sprite.displayHeight).setVisible(false).setScale(4)
 
-    exportTexture0.drawFrame('clothing_top_0', '__BASE', 0, 0, 1, this.color)
-    exportTexture1.drawFrame('clothing_top_1', '__BASE', 0, 0, 1, this.color)
-    exportTexture2.drawFrame('clothing_top_2', '__BASE', 0, 0, 1, this.color)
+    exportTexture0.drawFrame('clothing_top_0', '__BASE', 0, 0, 1, this.color2)
+    exportTexture1.drawFrame('clothing_top_1', '__BASE', 0, 0, 1, this.color1)
+    exportTexture2.drawFrame('clothing_top_2', '__BASE', 0, 0, 1, this.color2)
 
     exportTexture3.drawFrame('arm0', '__BASE')
-    exportTexture3.drawFrame('clothing_top_0', '__BASE', 0, 0, 1, this.color)
+    exportTexture3.drawFrame('clothing_top_0', '__BASE', 0, 0, 1, this.color2)
     exportTexture3.drawFrame('leg0', '__BASE')
     exportTexture3.drawFrame('body', '__BASE')
-    exportTexture3.drawFrame('clothing_top_1', '__BASE', 0, 0, 1, this.color)
+    exportTexture3.drawFrame('clothing_top_1', '__BASE', 0, 0, 1, this.color1)
     exportTexture3.drawFrame('leg2', '__BASE')
     exportTexture3.drawFrame('arm2', '__BASE')
-    exportTexture3.drawFrame('clothing_top_2', '__BASE', 0, 0, 1, this.color)
+    exportTexture3.drawFrame('clothing_top_2', '__BASE', 0, 0, 1, this.color2)
 
     exportTexture4.draw(poseSprite)
 
@@ -231,7 +295,7 @@ export default class Game extends Phaser.Scene {
       if (exportCount === 3) {
         console.log(exportResult)
         this.scale.setGameSize(1100, 700)
-        
+
         img.destroy()
         sprite.destroy()
         poseTexture.destroy()
